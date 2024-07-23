c536684aa19b9cf20e1f719b7bfd61003f12aca4
Gucci Gangnam <diviner02hive@icloud.com>
2024-07-24 00:44:39 +0700

    front styles built.  NEXT: work out logic

--- Retact-49-Letters/src/LandingPage.jsx
+++ Retact-49-Letters/src/LandingPage.jsx
@@ -6,38 +6,88 @@ import "./LandingPage.css"
 
 
 // COMPONENT 
-export const LandingPage = () => { 
-    return ( 
+export const LandingPage = () => {
+    return (
         <div className="LandingPage">
             <div className="Title">
-                <div className="Title-Box-Orange">
+                <div className="Title-Box-Black">
                     4
                 </div>
-                <div className="Title-Box-Orange">
+                <div className="Title-Box-Black">
                     9
                 </div>
-                <div className="Title-Box-Green">
+                <div className="Title-Box-White">
                     L
                 </div>
-                <div className="Title-Box-Green">
+                <div className="Title-Box-White">
                     E
                 </div>
-                <div className="Title-Box-Green">
+                <div className="Title-Box-White">
                     T
                 </div>
-                <div className="Title-Box-Green">
+                <div className="Title-Box-White">
                     T
                 </div>
-                <div className="Title-Box-Green">
+                <div className="Title-Box-White">
                     E
                 </div>
-                <div className="Title-Box-Green">
+                <div className="Title-Box-White">
                     R
                 </div>
-                <div className="Title-Box-Green">
+                <div className="Title-Box-White">
                     S
                 </div>
             </div>
+            <div className="Today-Date">
+                Monday 1st Janurary 2024
+            </div>
+            <div className="Play-Container">
+                <div className="Play-Container-Box">
+                    P
+                </div>
+                <div className="Play-Container-Box">
+                    L
+                </div>
+                <div className="Play-Container-Box">
+                    A
+                </div>
+                <div className="Play-Container-Box">
+                    Y
+                </div>
+            </div>
+
+            <div className="How-To-Play-Container">
+                <div className="How-To-Play-Container-Box">
+                    H
+                </div>
+                <div className="How-To-Play-Container-Box">
+                    O
+                </div>
+                <div className="How-To-Play-Container-Box">
+                    W
+                </div>
+                <div className="How-To-Play-Container-Box">
+                    T
+                </div>
+                <div className="How-To-Play-Container-Box">
+                    O
+                </div>
+                <div className="How-To-Play-Container-Box">
+                    P
+                </div>
+                <div className="How-To-Play-Container-Box">
+                    L
+                </div>
+                <div className="How-To-Play-Container-Box">
+                    A
+                </div>
+                <div className="How-To-Play-Container-Box">
+                    Y
+                </div>
+            </div>
+
+
+
         </div>
     )
 }
